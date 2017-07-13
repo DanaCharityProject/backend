1d0cc3cce1cb62f3d7e76292e60abe6f157d05ec
abetlen <abetlen@gmail.com>
2017-07-13 19:34:24 -0400

    Add test for post greeting method.
    This test highlights how  to use the flask test client to mock basic requests to an endpoint.

---
+++ test_app.py
@@ -0,0 +1,27 @@
+import app
+from app import create_app
+import pytest
+import json
+
+@pytest.fixture
+def client():
+    app = create_app()
+    flaskapp = app.app
+
+    flaskapp.testing = True
+    test_client = flaskapp.test_client()
+
+    ctx = flaskapp.app_context()
+    ctx.push()
+
+    yield test_client
+
+    ctx.pop()
+
+def test_post_greeting(client):
+    rv = client.post("/greeting", headers={"Content-type": "application/json"}, data=json.dumps({
+        "name": "john"
+    }))
+
+    assert rv.status_code == 200
+    assert "Hello john." in rv.get_data(as_text=True)
