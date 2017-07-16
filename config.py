ccbab5fbdec0ea29d294a5662c734344f27ef78d
abetlen <abetlen@gmail.com>
2017-07-16 17:48:25 -0400

    Add module documentation for config.

--- config.py
+++ config.py
@@ -1,3 +1,10 @@
+"""Configuration definition module
+
+This file is used to manage application confiugrations. These can include
+development, testing, staging, and production configurations. These different
+configuration may set different database uris, application setttings, error reporting
+modes, etc.
+"""
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
