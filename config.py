8625930e717d22b3a96e0d524019edb0fbc747b8
abetlen <abetlen@gmail.com>
2017-07-16 18:27:00 -0400

    Formatting and whitespace changes.

--- config.py
+++ config.py
@@ -9,16 +9,20 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config():
     DEBUG = False
     TESTING = False
     SECRET_KEY = os.environ.get("SECRET_KEY", "secret-key")
 
+
 class DevelopmentConfig(Config):
     DEBUG = True
 
+
 class TestingConfig(Config):
     TESTING = True
+
 
 config = {
     "development": DevelopmentConfig,
