b42fb5e0177d003761be8352733d398dbdb53b96
Marco <estebanm297@gmail.com>
2018-03-27 20:32:22 -0400

    Added a db set up of tables

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import os
 
-from app import create_app
+from app import create_app, db
 
 if __name__ == "__main__":
     app = create_app(os.environ.get("DANA_CONFIG", "development"))
+
+    with app.app.app_context():
+        db.drop_all()
+        db.create_all()
 
     app.run(port=5000, use_reloader=True, extra_files=["app/api.yml"])
