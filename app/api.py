20cf79a77dec1bd614707d2461d1e97e9f075045
abetlen <abetlen@gmail.com>
2018-05-24 00:10:49 -0400

    Updated references to removed Manager classes.

--- app/api.py
+++ app/api.py
@@ -1,12 +1,10 @@
-from __future__ import print_function
 import json
-from flask import g
 from connexion import NoContent
 from geopy.geocoders import Nominatim
 
-from .auth import auth, get_current_user, get_current_role
-from .models.user import User, UserManager, InvalidUserInfo, USER_ROLE_ADMIN, USER_ROLE_USER
-from .models.community_resource import CommunityResource, CommunityResourceManager, NoExistingCommunityResource, InvalidCommunityResourceInfo
+from .auth import auth, current_user, current_role
+from .models.user import User, InvalidUserInfo, USER_ROLE_ADMIN, USER_ROLE_USER
+from .models.community_resource import CommunityResource, NoExistingCommunityResource, InvalidCommunityResourceInfo
 from .validators import is_valid_password, is_valid_email, is_valid_phone_number, is_valid_community_resource_name
 import sys
 
@@ -14,50 +12,22 @@ import sys
 
 @auth.login_required
 def get_user():
-    return get_current_user().to_dict()
+    return current_user().to_dict()
 
 
 @auth.login_required
-def get_user_token():
-    return {"token": get_current_user().generate_auth_token().decode("ascii")}, 201
+def put_user(body):
+    user = current_user()
 
-
-def get_communityresource_info(body):
     try:
-        com_resource = CommunityResource.get_resource_by_charity_number(charity_number=body["charity_number"])
-    except NoExistingCommunityResource:
+        user.edit_user(body["username"])
+    except InvalidUserInfo:
         return NoContent, 500
+    return NoContent, 200
 
-    com_resource_dict = com_resource.to_dict()
-    return_dict = {
-        "name": com_resource_dict["name"],
-        "website": com_resource_dict["website"],
-        "address": com_resource_dict["address"],
-        "image_uri": com_resource_dict["image_uri"]
-    }
-
-    return return_dict, 200
-
-
-def get_nearby_communityresource(body):
-    lon, lat, rad = body["x"], body["y"], body["radius"]
-
-    resource_list = CommunityResource.get_resources_by_radius(lon, lat, rad)
-    res_info_list = []
-
-    for r in resource_list:
-        res_info_list.append(r.to_dict())
-
-    return res_info_list, 200
-
-
-#   ---------
 
 def post_user(body):
-    user = User(username=body["username"])
-    print("REEASDFASDFASDFASDFASDFASDFASDFASDF", file=sys.stderr)
-    user.password = body["password"]
-    user = User.add_user(user)
+    user = User.add_user(User.from_dict(body))
 
     if user is None:
         return NoContent, 409
@@ -65,11 +35,26 @@ def post_user(body):
     return user.to_dict(), 201
 
 
-# todo: make decorators for validation checks
-# todo: fix imminent mistakes in api.yml file
 @auth.login_required
-def post_communityresource_register(body):
-    if get_current_role != USER_ROLE_ADMIN:
+def put_user_password(body):
+    current_user().change_password(body["password"])
+    return NoContent, 200
+
+
+@auth.login_required
+def get_user_token():
+    return {"token": current_user().generate_auth_token().decode("ascii")}, 201
+
+
+def get_communityresource_list(longitude, latitude, radius):
+    community_resources = CommunityResource.get_resources_by_radius(longitude=longitude, latitude=latitude, radius=radius)
+
+    return [community_resource.to_dict() for community_resource in community_resources], 200
+
+
+@auth.login_required
+def post_communityresource(body):
+    if current_role() != USER_ROLE_ADMIN:
         return NoContent, 403
 
     charity_number = int(body["charity_number"])
@@ -81,12 +66,10 @@ def post_communityresource_register(body):
     website = body["website"]
     image_uri = body["image_uri"]
 
-    try:
-        (lon, lat) = __get_coordinates_from_address(address)
-    except:
-        return NoContent, 404
+    longitude, latitude = CommunityResource.coordinates_from_address(address)
 
-    resource = CommunityResource(charity_number=charity_number, name=name, y=lat, x=lon,
+    resource = CommunityResource(charity_number=charity_number, 
+                                 name=name, y=latitude, x=longitude,
                                  contact_name=contact_name, email=email,
                                  phone_number=phone_number, address=address,
                                  website=website, image_uri=image_uri,
@@ -100,46 +83,26 @@ def post_communityresource_register(body):
     return NoContent, 201
 
 
+def get_communityresource_detail(community_resource_id):
+    try:
+        community_resource = CommunityResource.get_resource_by_id(community_resource_id)
+    except NoExistingCommunityResource:
+        return NoContent, 500
+
+    return community_resource.to_dict(), 200
+
+
 @auth.login_required
-def put_community_resource_edit(body):
-    if get_current_role != USER_ROLE_ADMIN:
+def put_communityresource(community_resource_id, body):
+    if current_role() != USER_ROLE_ADMIN:
         return NoContent, 403
 
     try:
-        #_, (lat, lon) = geolocator.geocode(body["address"])
-        (lon, lat) = __get_coordinates_from_address(body["address"])
-    except:
-        return NoContent, 404
+        (longitude, latitude) = CommunityResource.coordinates_from_address(body["address"])
 
-    try:
-        CommunityResourceManager.edit_community_resource(int(body["charity_number"]), body["name"], lat, lon, body["contact_name"], body["email"], body["phone_number"], body["address"], body["website"], body["image_uri"])
+        CommunityResource.edit_community_resource(int(body["charity_number"]), body["name"], latitude, longitude, body["contact_name"], body["email"], body["phone_number"], body["address"], body["website"], body["image_uri"])
     except NoExistingCommunityResource:
         return NoContent, 404
     except InvalidCommunityResourceInfo:
         return NoContent, 400
     return NoContent, 200
-
-
-@auth.login_required
-def put_user_password(body):
-    UserManager.change_password(g.current_user, body["password"])
-    return NoContent, 200
-
-
-@auth.login_required
-def put_user_info(body):
-    user = g.current_user
-    try:
-        UserManager.edit_user(user.to_dict()["id"], body["username"])
-    except InvalidUserInfo:
-        return NoContent, 500
-    return NoContent, 200
-
-
-def __get_coordinates_from_address(body_address):
-    geolocator = Nominatim()
-    location = geolocator.geocode(body_address)
-    try:
-        return (location.longitude, location.latitude)  #x, y
-    except:
-        return NoContent, 500
--- app/auth.py
+++ app/auth.py
@@ -1,7 +1,7 @@
 from flask import g
 from flask_httpauth import HTTPBasicAuth, HTTPTokenAuth, MultiAuth
 
-from .models.user import UserManager
+from .models.user import User
 
 basic_auth = HTTPBasicAuth()
 token_auth = HTTPTokenAuth()
@@ -10,7 +10,7 @@ auth = MultiAuth(basic_auth, token_auth)
 
 @basic_auth.verify_password
 def verify_password(username, password):
-    user = UserManager.get_user_by_username(username)
+    user = User.get_user_by_username(username)
 
     if not user or not user.verify_password(password):
         return False
@@ -22,7 +22,7 @@ def verify_password(username, password):
 
 @token_auth.verify_token
 def verify_token(token):
-    user = UserManager.get_user_by_token(token)
+    user = User.get_user_by_token(token)
 
     if not user:
         return False
@@ -32,8 +32,8 @@ def verify_token(token):
 
     return True
 
-def get_current_user():
+def current_user():
     return g.current_user
 
-def get_current_role():
+def current_role():
     return g.current_role
