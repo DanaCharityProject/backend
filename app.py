ccf83c4c7c47f151e44bd06bce1abed5023f637a
“Anwesha2” <anwesha5294@gmail.com>
2018-05-26 10:13:15 -0400

    Added commented out posible input values for user and ommunity resource objects before switching branches

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import os
 import click
-
 from app import create_app, db
-
 
 app = create_app(os.environ.get("DANA_CONFIG", "development"))
 
@@ -14,6 +12,16 @@ def initdb():
     with app.app.app_context():
         db.drop_all()
         db.create_all()
+
+    '''session.add_all([ User(id=1, username='Eilonwy', password_hash='foobar'),
+    User(id=2, username='Taran', password_hash='prydain'),
+    User(id=3, username='Gurgi', password_hash='caer'),
+    ])'''
+        
+    '''session.add_all([ CommunityResource(id=1, charity_number=101, name="Charity1", y=43.6532, x=79.3832, contact_name="John Smith", email="JSmith@gmail.com",
+            phone_number="4165555555", address="1 Yonge ave", website="www.charity1.org", image_uri="", verified=false)
+            )])'''
+    #address line 1, address line 2 .... ?
 
 
 if __name__ == "__main__":
