bcc9543044f66637c4020d2ee00c76691f13c8b4
Marco <estebanm297@gmail.com>
2018-10-22 21:55:17 -0400

    Added testing script

---
+++ scripts/run_tests.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+docker-compose down
+docker-compose build
+docker-compose up -d db
+docker-compose up -d app
+docker-compose exec app flask create_db
+docker-compose exec app flask populate_db
+docker-compose run app python -m pytest
